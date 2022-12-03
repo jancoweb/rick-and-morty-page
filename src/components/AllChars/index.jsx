b594708aa47f33488187e704d0b9badd20ea881c
jancoweb <jancoweb@outlook.com>
2022-12-03 02:12:11 -0300

    page requests hotfix

--- src/components/AllChars/index.jsx
+++ src/components/AllChars/index.jsx
@@ -1,20 +1,20 @@
 import { useGlobalContext } from "../../context/ContextProvider";
 import api from "../../services/api";
 import './style.css'
-import { useEffect } from "react";
 
 export default function AllChars() {
   const { allChars, setAllChars, page, setPage, setChar } = useGlobalContext();
 
   async function getData(page) {
+    setPage(page)
     if (page > 42) {
       setPage(1)
-    }
-    if (page < 1) {
+    } else if (page < 1) {
       setPage(42)
     }
+
     try {
-      let response = await api.get(`/character?page=${page}`);
+      let response = await api.get(`/character?page=${page > 42 ? 1 : page < 1 ? 42 : page}`);
       let data = response.data.results;
       setAllChars(data)
     } catch (error) {
@@ -32,14 +32,10 @@ export default function AllChars() {
     }
   }
 
-  useEffect(() => {
-    getData(page)
-  }, [page])
-
   return (
     <>
       <div className="all-wrapper">
-        <button className="previous" onClick={() => getData(setPage(page - 1))}>{'<'}</button>
+        <button className="previous" onClick={() => getData(page - 1)}>{'<'}</button>
         <div className="all-chars-container">
           {allChars.map(char => (
             <div className="each" key={char.id} onClick={() => { handleSelect(char.id) }}>
@@ -48,7 +44,7 @@ export default function AllChars() {
             </div>
           ))}
         </div>
-        <button className="next" onClick={() => getData(setPage(page + 1))}>{'>'}</button>
+        <button className="next" onClick={() => getData(page + 1)}>{'>'}</button>
       </div>
       <h1 className="page-number">{page}</h1>
     </>
