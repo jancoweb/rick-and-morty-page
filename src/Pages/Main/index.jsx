09e88cfdda8cf8c9fa92c6c9758574fdd48c366d
jancoweb <jancoweb@outlook.com>
2022-12-02 21:58:39 -0300

    bg gif

--- src/Pages/Main/index.jsx
+++ src/Pages/Main/index.jsx
@@ -10,6 +10,7 @@ export default function MainPage() {
 
   return (
     <div className="main-page-wrapper">
+      <img src="https://images.squarespace-cdn.com/content/v1/57433cc43c44d8db180cd231/1546962591286-A0IL1S8JTZKQMS0MOEJP/rick-animated-head-Burp.gif?format=2500w" style={{ display: char ? 'none' : '' }} />
       <NavBar />
       {notFound &&
         <div className="not-found-message">
